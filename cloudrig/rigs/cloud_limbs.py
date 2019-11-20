2635d542a4e2c02444deb39e854c6902d78c5d33
Met at work <metssfm@gmail.com>
2019-11-20 16:48:21 +0100

    reorganize

--- cloudrig/rigs/cloud_limbs.py
+++ cloudrig/rigs/cloud_limbs.py
@@ -25,25 +25,35 @@ from rigify.utils.errors import MetarigError
 from rigify.utils.rig import connected_children_names
 from rigify.utils.naming import make_derived_name
 from rigify.utils.widgets_basic import create_bone_widget
+from rigify.utils.bones import BoneDict, BoneUtilityMixin
 
 from rigify.base_rig import BaseRig, stage
 
-
+# Registerable rig template classes MUST be called exactly "Rig"!!!
+# (This class probably shouldn't be registered in the future)
 class Rig(BaseRig):
-    """ A "copy_chain" rig.  All it does is duplicate the original bone chain
-        and constrain it.
-        This is a control and deformation rig.
+    """ Base for CloudRig arms and legs.
     """
+    # overrides BaseRig.find_org_bones.
+    def find_org_bones(self, bone):
+        """Populate self.bones.org."""
+        # For now we just grab all connected children of our main bone and pout it in self.bones.org.main.
+        print("find org bones")
+        return BoneDict(
+            main=[bone.name] + connected_children_names(self.obj, bone.name),
+        )
+
     def initialize(self):
         super().initialize()
-
-        """ Gather and validate data about the rig.
-        """
+        """Gather and validate data about the rig."""
         self.type = self.params.type
+        print("initialize")
 
+    # DSP bones - Display bones at the mid-point of each bone to use as display transforms.
     @stage.generate_bones
-    def create_bones(self):
-        for b in self.bones.org:
+    def create_dsp_bones(self):
+        print("create dsp bones")
+        for b in self.bones.org.main:
             # Let's just create some new bones for each existing bone with name dependent on the param.
             new_name = self.type
             self.copy_bone(b, new_name)
@@ -56,6 +66,7 @@ class Rig(BaseRig):
         """ Add the parameters of this rig type to the
             RigifyParameters PropertyGroup
         """
+        print("add params")
         params.type = bpy.props.EnumProperty(name="Type",
         items= (
             ("ARM", "Arm", "Arm"),
---
+++ cloudrig/rigs/utils.py
@@ -0,0 +1,7 @@
+def make_dsp_bone(obj, parent):
+    # Create a new bone.
+    # Name is the original bone in full prefixed with DSP-.
+    # Assign to DSP group.
+    # Parent it to source bone.
+    # Return it so it can be moved.
+    pass
