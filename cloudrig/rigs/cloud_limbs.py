f5205688e377c7b1d18659bbd4786ce6ee1b202d
Met at work <metssfm@gmail.com>
2019-11-25 18:32:25 +0100

    Start implementing IK

--- cloudrig/rigs/cloud_limbs.py
+++ cloudrig/rigs/cloud_limbs.py
@@ -16,7 +16,7 @@
 #
 #======================= END GPL LICENSE BLOCK ========================
 
-# <pep8 compliant>
+# <pep8 compliant> (TODO: make it so)
 
 import bpy
 from bpy.props import *
@@ -36,13 +36,20 @@ from rigify.base_rig import BaseRig, stage
 
 from .cloud_utils import load_widget, make_name, slice_name
 
+# Ideas:
+# I'd rather abstract bones in a way where I don't have to worry about being in pose or edit mode at all. Ie, store all bone properties in an abstract class that mimics Blender's internal bone data structures(while combining them all)
+# But I'm not sure if it's worth the hassle or if I would run into any headaches.
+# (This is the same idea that we arleady did for managing Drivers, which worked out pretty well, and these could also be combined)
+# (We would add a "drivers" field to our ID class, which would store a list of drivers, or whatever extra info a driver would need to be realized on that ID when it is made into a real one.)
+# And then I guess we would have to do the same for constraints.
+# There could also be an Armature abstraction, just for the sake of storing a list of bones. That way, armature.make_real() can make the bones real without switching between edit and pose mode multiple times.
+
+
 # Registerable rig template classes MUST be called exactly "Rig"!!!
 # (This class probably shouldn't be registered in the future)
 class Rig(BaseRig):
 	""" Base for CloudRig arms and legs.
 	"""
-	# NOTE: The rigging shouldn't rely on ORG bones unless neccessary, so that we can delete them at the end of the generation process. There's no need for the generated rig to contain the original metarig.
-
 	# overrides BaseRig.find_org_bones.
 	def find_org_bones(self, bone):
 		"""Populate self.bones.org."""
@@ -54,9 +61,10 @@ class Rig(BaseRig):
 	def initialize(self):
 		super().initialize()
 		"""Gather and validate data about the rig."""
-		self.type = self.params.type
-
 		assert len(self.bones.org.main)==3, "Limb bone chain must consist of exactly 3 connected bones."
+
+		self.bones.parent = self.get_bone(self.base_bone).parent.name
+		self.type = self.params.type
 
 	# DSP bones - Display bones at the mid-point of each bone to use as display transforms for FK.
 	# TODO: This should be in a shared place like utils or something.
@@ -78,17 +86,72 @@ class Rig(BaseRig):
 	@stage.generate_bones
 	def generate_everything(self):
 		# Let's create a root bone for the rig, for giggles and sanity.
-		root_name = "Root_Arm" if self.params.type=='ARM' else "Root_Leg"
-		root_name = make_name( base=root_name, suffixes=slice_name(self.base_bone)[2] )
-		self.bones.ctrl.root = self.copy_bone(self.base_bone, root_name)
-		root_bone = self.get_bone(self.bones.ctrl.root)
-		root_bone.parent = self.get_bone(self.base_bone).parent
+		# TODO: This might've been pointless. We'll keep it around for now.
+		# root_name = "Root_Arm" if self.params.type=='ARM' else "Root_Leg"
+		# root_name = make_name( base=root_name, suffixes=slice_name(self.base_bone)[2] )
+		# self.bones.ctrl.root = self.copy_bone(self.base_bone, root_name)
+		# root_bone = self.get_bone(self.bones.ctrl.root)
+		# root_bone.parent = self.get_bone(self.base_bone).parent
 
 		self.generate_fk(self.bones.org.main)
-		self.generate_ik_controls(self.bones.org.main)
+		self.generate_ik(self.bones.org.main)
+		self.generate_stretchy(self.bones.org.main)
+		self.generate_deform(self.bones.org.main)
 
-	def generate_ik_controls(self, chain):
-		pass
+	def generate_ik(self, chain):
+		# What we need:
+		# IK Chain (equivalents to ORG, so 3 of these) - Make sure IK Stretch is enabled on first two, and they are parented and connected to each other.
+		# IK Controls: Wrist, Wrist Parent(optional)
+		# IK-STR- bone with its Limit Scale constraint set automagically somehow.
+		# IK Pole target and line, somehow automagically placed.
+		
+		# Create IK Chain (first two bones)
+		for i, bn in enumerate(chain[:-1]):
+			ik_name = bn.replace("ORG", "IK")
+			self.copy_bone(bn, ik_name)
+			ik_bone = self.get_bone(ik_name)
+
+			if 'ik' not in self.bones.mch:
+				self.bones.mch.ik = []
+			
+			if i > 0:
+				ik_bone.parent = self.get_bone(self.bones.mch.ik[-1])
+			else:
+				ik_bone.parent = self.get_bone(self.bones.parent)
+			
+			self.bones.mch.ik.append(ik_name)
+
+		# Create IK control(s) (Wrist/Ankle)
+		self.bones.ctrl.ik = []
+		bn = chain[-1]
+		ik_name = bn.replace("ORG", "IK")
+		self.copy_bone(bn, ik_name)
+		ik_bone = self.get_bone(ik_name)
+		self.bones.ctrl.ik.append(ik_name)
+		if self.params.double_ik_control:
+			sliced = slice_name(ik_name)
+			sliced[0].append("P")
+			parent_name = make_name(*sliced)
+			self.copy_bone(ik_name, parent_name)
+			self.bones.ctrl.ik.append(parent_name)
+			parent_bone = self.get_bone(parent_name)
+			ik_bone.parent = parent_bone
+		
+		# Stretch mechanism
+		sliced = slice_name(ik_name)
+		sliced[0].append("STR")
+		str_name = self.bones.mch.ik_stretch = make_name(*sliced)
+		self.copy_bone(self.bones.mch.ik[0], str_name)
+		str_bone = self.get_bone(str_name)
+		str_bone.tail = ik_bone.head
+		str_bone.bbone_x = 20
+
+		sliced[0].append("TIP")
+		tip_name = self.bones.mch.ik_stretch_tip = make_name(*sliced)
+		self.copy_bone(ik_name, tip_name)
+		tip_bone = self.get_bone(tip_name)
+		tip_bone.parent = ik_bone
+
 
 	def generate_fk(self, chain):
 		for i, bn in enumerate(chain):
@@ -134,6 +197,12 @@ class Rig(BaseRig):
 		fk_bone.parent = hng_bone
 		#hng_bone.parent = self.get_bone(self.bones.ctrl.root)
 
+	def generate_stretchy(self, chain):
+		pass
+
+	def generate_deform(self, chain):
+		pass
+
 	@stage.configure_bones
 	def configure_rot_modes(self):
 		for ctb in self.bones.ctrl.flatten():
@@ -152,7 +221,7 @@ class Rig(BaseRig):
 		target2 = con_arm.targets.new()
 		target1.target = target2.target = self.obj
 		target1.subtarget = 'root'
-		target2.subtarget = self.get_bone(self.base_bone).parent.name
+		target2.subtarget = self.bones.parent
 
 		# TODO: Create UI for custom property, and maybe store it elsewhere. 
 		# I think I like it when all options are displayed all the time, but it can be limiting, 
@@ -176,16 +245,31 @@ class Rig(BaseRig):
 
 		con_copyloc = self.make_constraint(hng, 'COPY_LOCATION')
 		con_copyloc.target = self.obj
-		con_copyloc.subtarget = self.get_bone(self.base_bone).parent.name
+		con_copyloc.subtarget = self.bones.parent
 		con_copyloc.head_tail = 1
+
+	@stage.configure_bones
+	def configure_ik(self):
+		ik_bone_name = self.bones.mch.ik[-1]
+		con_ik = self.make_constraint(ik_bone_name, 'IK')
+		con_ik.target = self.obj
+		con_ik.subtarget = self.bones.ctrl.ik[0] # TODO: This target would actually be a MCH bone that's parented to the IK-STR MCH bone.
+		# TODO pole target.
+
+		str_bone = self.get_bone(self.bones.mch.ik_stretch)
+		tip_bone = self.get_bone(self.bones.mch.ik_stretch_tip)
+
+		con_str = self.make_constraint(str_bone.name, 'STRETCH_TO')
+		con_str.target = self.obj
+		con_str.subtarget = tip_bone.name
 
 	@stage.finalize
 	def configure_display(self):
 		# DSP bones
-		if self.params.display_middle:
-			for i, fk_name in enumerate(self.bones.ctrl.fk):
-				fk_bone = self.get_bone(fk_name)
-				fk_bone.custom_shape = load_widget("FK_Limb")
+		for i, fk_name in enumerate(self.bones.ctrl.fk):
+			fk_bone = self.get_bone(fk_name)
+			fk_bone.custom_shape = load_widget("FK_Limb")
+			if self.params.display_middle:
 				try:
 					dsp_name = "DSP-"+fk_name
 					dsp_bone = self.get_bone(dsp_name)
@@ -196,12 +280,17 @@ class Rig(BaseRig):
 					# If bone was not found, do nothing.
 					pass
 
-				if i == 0 and self.params.double_first_control:
-					fk_bone.custom_shape_scale = 1.1
+			if i == 0 and self.params.double_first_control:
+				fk_bone.custom_shape_scale = 1.1
 		
-		root_bone = self.get_bone(self.bones.ctrl.root)
-		root_bone.custom_shape = load_widget("Cube")
-		root_bone.custom_shape_scale = 0.5
+		for i, ik in enumerate(self.bones.ctrl.ik):
+			ik_bone = self.get_bone(ik)
+			ik_bone.custom_shape = load_widget("Hand_IK")
+			ik_bone.custom_shape_scale = 1 + 0.1*i
+		
+		# root_bone = self.get_bone(self.bones.ctrl.root)
+		# root_bone.custom_shape = load_widget("Cube")
+		# root_bone.custom_shape_scale = 0.5
 
 		# Armature display settings
 		self.obj.display_type = 'SOLID'
@@ -223,8 +312,13 @@ class Rig(BaseRig):
 			),
 		)
 		params.double_first_control = BoolProperty(
-			name="Double First Control", 
+			name="Double First FK Control", 
 			description="The first FK control has a parent control. Having two controls for the same thing can help avoid interpolation issues when the common pose in animation is far from the rest pose",
+			default=True,
+		)
+		params.double_ik_control = BoolProperty(
+			name="Double IK Control", 
+			description="The IK control has a parent control. Having two controls for the same thing can help avoid interpolation issues when the common pose in animation is far from the rest pose",
 			default=True,
 		)
 		params.display_middle = BoolProperty(
@@ -241,5 +335,7 @@ class Rig(BaseRig):
 		r.prop(params, "type")
 		r = layout.row()
 		r.prop(params, "double_first_control")
+		r = layout.row()
+		r.prop(params, "double_ik_control")
 		r = layout.row()
 		r.prop(params, "display_middle")
