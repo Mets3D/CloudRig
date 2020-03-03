09de36fc39aaea06f1c69c2d23a7c5fdb63d8910
Met at work <metssfm@gmail.com>
2020-03-03 14:45:16 +0100

    Spline IK rig implemented and working pretty well.

--- cloudrig/rigs/cloud_spline_ik.py
+++ cloudrig/rigs/cloud_spline_ik.py
@@ -19,8 +19,25 @@ class CloudSplineIKRig(CloudBaseRig):
 		self.params.cap_control = True
 		self.params.shape_key_helpers=False
 
+	def fit_on_bone_chain(self, chain, length):
+		"""On a bone chain, find the point a given length down the chain. Return its position and direction."""
+		length_cumultative = 0
+		for b in chain:
+			if length_cumultative + b.length > length:
+				length_remaining = length - length_cumultative
+				direction = b.vec.normalized()
+				loc = b.head + direction * length_remaining
+				return (loc, direction)
+			else:
+				length_cumultative += b.length
+		
+		length_remaining = length - length_cumultative
+		direction = chain[-1].vec.normalized()
+		loc = chain[-1].tail + direction * length_remaining
+		return (loc, direction)
+
 	@stage.prepare_bones
-	def prepare_spline_deform(self):
+	def create_def_chain(self):
 		self.def_bones = []
 		segments = self.params.segments
 
@@ -32,8 +49,6 @@ class CloudSplineIKRig(CloudBaseRig):
 				number = str(i+1) if segments > 1 else ""
 				def_name = make_name(sliced[0], sliced[1] + number, sliced[2])
 
-				# org_bone = self.get_bone(org_name)
-				# org_vec = org_bone.tail-org_bone.head
 				unit = org_bone.vec / segments
 
 				def_bone = self.bone_infos.bone(
@@ -42,10 +57,6 @@ class CloudSplineIKRig(CloudBaseRig):
 					head = org_bone.head + (unit * i),
 					tail = org_bone.head + (unit * (i+1)),
 					roll = org_bone.roll,
-					# bbone_handle_type_start = 'TANGENT',
-					# bbone_handle_type_end = 'TANGENT',
-					# bbone_segments = bbone_segments,
-					# inherit_scale = 'NONE',
 				)
 
 				if len(self.def_bones) > 0:
@@ -55,20 +66,27 @@ class CloudSplineIKRig(CloudBaseRig):
 				
 				self.def_bones.append(def_bone)
 
-
-	@stage.finalize
-	def create_curve(self):
-		# We want to create a curve that starts and ends where our bone chain does, but where are the intermittent curve points?
-		# Where the intermittent bones are does not really matter. We just want to spread our curve points out equally along the curvature of the bones.
-		# But what if the bones aren't dense enough to have a curvature? Do we want to account for that?
-		# I guess let's say that for now, we do not.
-
-		# Potential solution, also bringing bbones back: Sort of self-solving, actually. If STR- bones are owned by ORG, and ORG- bones will have the spline IK constraint... The STR- bones might be rotated in just the right way for the BBones to follow the spline IK curve?
-
-		# Just find the sum of the lengths of the bones. Then divide that by num_controls to get where on the bones' length we want each control to sit.
-		
+	@stage.prepare_bones
+	def create_hook_controls(self):
 		sum_bone_length = sum([b.length for b in self.org_chain])
 		length_unit = sum_bone_length / (self.params.num_controls-1)
+
+		self.hook_bones = []
+		for i in range(0, self.params.num_controls):
+			point_along_chain = i * length_unit
+			loc, direction = self.fit_on_bone_chain(self.org_chain, point_along_chain)
+			hook_ctr = self.bone_infos.bone(
+				name = "Hook_" + str(i).zfill(2),
+				head = loc,
+				tail = loc + direction*self.scale,
+				custom_shape = self.load_widget("CurveHandle")
+			)
+			self.hook_bones.append(hook_ctr)
+
+	def create_curve(self):
+		sum_bone_length = sum([b.length for b in self.org_chain])
+		length_unit = sum_bone_length / (self.params.num_controls-1)
+		handle_length = length_unit / self.params.curve_handle_ratio
 		
 		# Find or create Bezier Curve object for this rig.
 		curve_name = self.obj.name.replace("RIG-", "CUR-")
@@ -78,8 +96,16 @@ class CloudSplineIKRig(CloudBaseRig):
 			bpy.data.objects.remove(curve_ob)	# What's not so cool about this is that if anything in the scene was referencing this curve, that reference gets broken.
 
 		bpy.ops.curve.primitive_bezier_curve_add(radius=0.2, location=(0, 0, 0))
-		curve_ob = bpy.context.view_layer.objects.active
+		self.obj.select_set(True)
+
+		bpy.ops.object.mode_set(mode='EDIT')
+		bpy.ops.curve.select_all(action='DESELECT')
+		self.curve = curve_ob = bpy.context.view_layer.objects.active
 		curve_ob.name = curve_name
+		curve_ob.lock_location = [True, True, True]
+		curve_ob.lock_rotation = [True, True, True]
+		curve_ob.lock_scale = [True, True, True]
+		curve_ob.lock_rotation_w = True
 
 		# Place the first and last bezier points to the first and last bone.
 		spline = curve_ob.data.splines[0]
@@ -88,42 +114,52 @@ class CloudSplineIKRig(CloudBaseRig):
 		# Add the necessary number of curve points
 		points.add( self.params.num_controls-len(points) )
 
-		def fit_on_bone_chain(chain, length):
-			"""On a bone chain, find the point a given length down the chain."""
-			length_cumultative = 0
-			for b in chain:
-				if length_cumultative + b.length > length:
-					length_remaining = length - length_cumultative
-					return b.head + b.vec.normalized()*length_remaining
-				else:
-					length_cumultative += b.length
-			return chain[-1].tail.copy()
-
 		# Place the curve points along the bone chain.
 		for i, p in enumerate(points):
-			p.co = fit_on_bone_chain(self.org_chain, i*length_unit)
-			print(p.co)
+			point_along_chain = i * length_unit
 
-		# points[0].co = self.org_chain[0].head.copy()
-		# points[1].co = self.org_chain[-1].tail.copy()
-
-		# points[0].handle_right = points[0].co + self.org_chain[0].vec * 1 # TODO: Some length factor, based on length_unit probably.
-		# points[0].handle_left = points[0].co - self.org_chain[0].vec * 1 # TODO: Some length factor, based on length_unit probably.
-		# points[1].handle_right = points[1].co + self.org_chain[-1].vec
-		# points[1].handle_left = points[1].co - self.org_chain[-1].vec
-		# Then we iterate params.num_controls number of times.
-			# We subdivide the curve with the first and "latest" curve point selected. In the first iteration, that's the last curve point, from then on, it's the curve point that was created in the previous iteration.
-			# A single new curve point should be created and selected - save it to a list, 
-		
-		for p in points:
 			p.radius = 0.2
 			p.handle_left_type = 'ALIGNED'
 			p.handle_right_type = 'ALIGNED'
 
+			p.co = self.fit_on_bone_chain(self.org_chain, point_along_chain)[0]
+			p.handle_right = self.fit_on_bone_chain(self.org_chain, point_along_chain + handle_length)[0]
+			p.handle_left  = self.fit_on_bone_chain(self.org_chain, point_along_chain - handle_length)[0]
+
+			p.select_control_point = True
+			p.select_left_handle = True
+			p.select_right_handle = True
+
+			# Set active bone
+			hook_b = self.obj.data.bones.get(self.hook_bones[i].name)
+			self.obj.data.bones.active = hook_b
+
+			bpy.ops.object.hook_add_selob(use_bone=True)
+
+			p.select_control_point = False
+			p.select_left_handle = False
+			p.select_right_handle = False
+
+
 		bpy.ops.object.mode_set(mode='OBJECT')
+		bpy.context.scene.collection.objects.unlink(curve_ob)
+		self.generator.collection.objects.link(curve_ob)
+		curve_ob.hide_viewport=True
 		bpy.context.view_layer.objects.active = self.obj
 		self.obj.select_set(True)
 
+	def configure_bones(self):
+		self.create_curve()
+
+		# Add constraint to deform chain
+		self.def_bones[-1].add_constraint(self.obj, 'SPLINE_IK', 
+			use_curve_radius=False,
+			chain_count=len(self.def_bones),
+			target=self.curve,
+			true_defaults=True
+		)
+
+		super().configure_bones()
 
 	##############################
 	# Parameters
@@ -135,11 +171,22 @@ class CloudSplineIKRig(CloudBaseRig):
 		"""
 		super().add_parameters(params)
 		
+		params.match_controls_to_bones = BoolProperty(
+			name="Match Controls to Bones",
+			description="Hook controls will be created at each bone, instead of being equally distributed across the length of the chain",
+			default=True
+		)
+		params.curve_handle_ratio = FloatProperty(
+			name="Curve Handle Length Ratio",
+			description="Increasing this will result in shorter curve handles, resulting in a sharper curve.",
+			default=3.0,
+		)
 		params.num_controls = IntProperty(
 			name="Number of controls",
 			description="Number of controls that will be spaced out evenly across the entire chain",
 			default=3,
-			min=3
+			min=3,
+			max=99
 		)
 		params.segments = IntProperty(
 			name="Subdivide bones",
@@ -155,7 +202,11 @@ class CloudSplineIKRig(CloudBaseRig):
 		super().parameters_ui(layout, params)
 
 		layout.prop(params, "segments")
-		layout.prop(params, "num_controls")
+		layout.prop(params, "curve_handle_ratio")
+
+		layout.prop(params, "match_controls_to_bones")	# TODO implement this.
+		if not params.match_controls_to_bones:
+			layout.prop(params, "num_controls")
 
 class Rig(CloudSplineIKRig):
 	pass
