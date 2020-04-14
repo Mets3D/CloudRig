3133467fa1a4ebce773875a7327a860411b9cde1
Mets3D <metssfm@gmail.com>
2020-04-14 16:19:57 +0200

    Unregister works, I think!

--- __init__.py
+++ __init__.py
@@ -11,6 +11,8 @@ import bpy, os
 from bpy.props import *
 from rigify.feature_set_list import *
 
+# We overwrite Rigify's Remove External Feature Set operator such that when a feature set is removed, its unregister() is called.
+# This is a hack or rather a test, and ideally this would at some point be added to Rigify itself.
 class DATA_OT_rigify_remove_and_unregister_feature_set(bpy.types.Operator):
 	bl_idname = "wm.rigify_remove_feature_set"
 	bl_label = "Remove External Feature Set"
@@ -28,25 +30,22 @@ class DATA_OT_rigify_remove_and_unregister_feature_set(bpy.types.Operator):
 
 	def execute(self, context):
 		# First, unregister the feature set, if there is an unregister() in its __init__.py.
-		
-		module = get_module_safe(self.featureset)
-		print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA REMOVING FEATURE SET")
-		if module and hasattr(module, 'unfuck') and callable(module.unfuck):
-			print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA HAS unfuck")
-			module.unfuck()
+		featureset = self.featureset
+
+		module = get_module_safe(featureset)
+		if module and hasattr(module, 'unregister') and callable(module.unregister):
+			module.unregister()
 
 		addon_prefs = context.preferences.addons['rigify'].preferences
 		rigify_config_path = get_install_path()
 		if rigify_config_path:
-			set_path = os.path.join(rigify_config_path, self.featureset)
+			set_path = os.path.join(rigify_config_path, featureset)
 			if os.path.exists(set_path):
 				rmtree(set_path)
 
 		addon_prefs.update_external_rigs(force=True)
 		return {'FINISHED'}
 
-
-# TODO: Not sure how to get Rigify to call our register() and unregister() for us.
 from bpy.utils import register_class
 from bpy.utils import unregister_class
 
@@ -59,7 +58,7 @@ def register():
 	unregister_class(DATA_OT_rigify_remove_feature_set)
 	register_class(DATA_OT_rigify_remove_and_unregister_feature_set)
 
-def unfuck():
+def unregister():
 	print("Unregistering CloudRig...")
 	regenerate_rigify_rigs.unregister()
 	refresh_drivers.unregister()
@@ -69,4 +68,5 @@ def unfuck():
 	unregister_class(DATA_OT_rigify_remove_and_unregister_feature_set)
 	register_class(DATA_OT_rigify_remove_feature_set)
 
+# TODO: This register is only called when Blender is started, NOT when the feature set is added to Rigify. I'm not sure how that would be achieved.
 register()
