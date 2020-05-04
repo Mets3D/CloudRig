f05b18026612e2b4e20e509fd2b8b0ba99d7508c
Met at work <metssfm@gmail.com>
2020-05-04 15:09:39 +0200

    fix stretch constraints better

--- definitions/bone.py
+++ definitions/bone.py
@@ -460,10 +460,12 @@ class BoneInfo(ID):
 						for prop in copy:
 							if prop in tinfo:
 								setattr_safe(target, prop, tinfo[prop])
-				if con_type == 'STRETCH_TO':
-					c.rest_length = 0
 				elif(hasattr(c, key)):
 					setattr_safe(c, key, value)
+
+				# Fix stretch constraints
+				if c.type == 'STRETCH_TO':
+					c.rest_length = 0
 		
 		# Custom Properties.
 		for key, prop in self.custom_props.items():
