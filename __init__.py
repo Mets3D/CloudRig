755b2579d9f5d08bd0ce4278d598081cccbb1545
Met at work <metssfm@gmail.com>
2020-05-25 19:32:57 +0200

    fiddling with register_manual_map()

--- __init__.py
+++ __init__.py
@@ -11,8 +11,18 @@ from . import ui
 import bpy, os
 from bpy.props import StringProperty
 
+
+# This allows you to right click on a button and link to documentation
+def cloudrig_manual_map():
+	url_manual_prefix = "https://gitlab.com/blender/CloudRig/-/wikis/"
+	url_manual_mapping = (
+		("bpy.ops.pose.cloudrig_generate", "Custom-Properties"),
+		("cloudrigproperties*", "")
+	)
+	return url_manual_prefix, url_manual_mapping
+
 def register():
-	from bpy.utils import register_class
+	from bpy.utils import register_class, register_manual_map
 	regenerate_rigify_rigs.register()
 	refresh_drivers.register()
 	mirror_rigify.register()
@@ -20,9 +30,13 @@ def register():
 	cloud_generator.register()
 	ui.register()
 
+	register_manual_map(cloudrig_manual_map)
+
 def unregister():
 	"""Hopefully one day Rigify will call unregister() for feature sets that are removed. Until then, this is useless."""
-	from bpy.utils import unregister_class
+	from bpy.utils import unregister_class, unregister_manual_map
+	unregister_manual_map(cloudrig_manual_map)
+
 	regenerate_rigify_rigs.unregister()
 	refresh_drivers.unregister()
 	mirror_rigify.unregister()
